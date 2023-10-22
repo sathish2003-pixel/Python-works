48d88aa2a6e7597c7c6c1ed99230338202e137df
sathish2003-pixel <satheshsathish2206@gmail.com>
2023-10-22 20:19:09 +0530

    If condition created

---
+++ ifcondition.py
@@ -0,0 +1,9 @@
+name=input("Enter Your Name:")
+age=int(input("Enter your age:"))
+if age > 18:
+    print(name,"Your are eligible for voting")
+else:
+    print(name,"You are not eligible for voting")
+
+print(name,age)
+
