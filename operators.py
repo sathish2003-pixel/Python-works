8b711a6921b6a6e1ed12afb3cceafabbb39639d3
sathish2003-pixel <satheshsathish2206@gmail.com>
2023-10-25 18:01:01 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,28 @@
+# The python operators are listed below 
+""""+	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y"""""
+
+a=int(input("Entet the number:"))
+b=int(input("Enter the number:"))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+floordiv=a//b
+expo=a**b
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(floordiv)
+print(expo)
+
+
